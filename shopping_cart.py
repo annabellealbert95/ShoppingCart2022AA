4a417977faff2dcf8728693d434bd717381494d6
annabellealbert95 <80716097+annabellealbert95@users.noreply.github.com>
2022-02-15 09:37:06 -0500

    seperating capture and display process

--- shopping_cart.py
+++ shopping_cart.py
@@ -29,23 +29,32 @@ products = [
 #
 
 total_price = 0
-
+selected_ids = []
 
 while True: 
     selected_id = input("Please input a product identifier: ")  #> "g" (string)
 
     if selected_id == "DONE": 
         break
-    else: 
+    else:  
+        #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        #matching_product = matching_products[0]
+        #total_price = total_price + matching_product["price"]
+        #print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
+        selected_ids.append(selected_id)
+
+#
+#INFO DISPLAY/OUTPUT
+#
+
+#print(selected_ids)
+
+for selected_id in selected_ids:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
         total_price = total_price + matching_product["price"]
         print("Selected Product: " + matching_product["name"] + " " + str(matching_product["price"]))
 
-
-#
-#INFO DISPLAY/OUTPUT
-#
 
 print("Total Price: " + str(total_price)) #todo format as USD
 
